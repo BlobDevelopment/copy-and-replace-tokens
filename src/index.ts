5674eb928dbd84800ecc5da574ed0b7b912034aa
Daniel Walsh <walshydev@gmail.com>
2020-06-30 03:50:58 +0100

    Package

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import * as fs from 'fs';
-import * as core from '@actions/core'
+import { existsSync,readFileSync, writeFileSync } from 'fs';
+import { getInput, setFailed } from '@actions/core'
 
-const source = core.getInput('source') || 'test/a.txt';
-const target = core.getInput('target') || 'test/b.txt';
+const source = getInput('source') || 'test/a.txt';
+const target = getInput('target') || 'test/b.txt';
 
 //process.env['{TEST}'] = 'Testing123';
 
 function run() {
-  if (!fs.existsSync(source)) {
-    core.setFailed(`${source} does not exist`);
+  if (!existsSync(source)) {
+    setFailed(`${source} does not exist`);
     return;
   }
 
   //fs.copyFileSync(source, target);
 
-  let fileAsString = fs.readFileSync(source, 'utf8');
+  let fileAsString = readFileSync(source, 'utf8');
 
   Object.keys(process.env).forEach(key => {
     if (key.charAt(0) === '{' && key.charAt(key.length - 1) === '}') {
@@ -23,7 +23,7 @@ function run() {
   });
 
   console.log(`Wrote replacements from ${source} into ${target}`);
-  fs.writeFileSync(target, fileAsString);
+  writeFileSync(target, fileAsString);
 }
 
 run();
