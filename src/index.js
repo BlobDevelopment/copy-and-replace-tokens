3dca2b77b327018940b794021989624f36b97439
Daniel Walsh <walshydev@gmail.com>
2021-12-26 00:01:22 +0000

    Finish

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import { existsSync,readFileSync, writeFileSync } from 'fs';
 import { getInput, setFailed } from '@actions/core'
 
-const source = getInput('source') || 'test/a.txt';
+const source = getInput('source');
 const target = getInput('target') || source;
-
-//process.env['TEST'] = 'Testing123';
 
 function run() {
   if (!existsSync(source)) {
@@ -15,12 +13,12 @@ function run() {
   let fileAsString = readFileSync(source, 'utf8');
 
   for (const key of Object.keys(process.env)) {
-    console.log(key + ' = ' + process.env[key]);
     fileAsString = fileAsString.replaceAll(`{${key}}`, process.env[key]);
   }
 
   console.log(`Wrote replacements from ${source} into ${target}`);
   writeFileSync(target, fileAsString);
+  console.log('New file:\n' + fileAsString);
 }
 
 run();
